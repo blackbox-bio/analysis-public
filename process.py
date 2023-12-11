3da842ea1c18d4e2ccc27f07207b41e9b8115767
Zihe Alex Zhang <zihe_zhang@g.harvard.edu>
2023-12-11 16:34:00 -0500

    debug

--- process.py
+++ process.py
@@ -3,8 +3,8 @@ from utils import *
 dlc_postfix = "DLC_resnet50_arcteryx500Nov4shuffle1_350000"
 
 # Function to process a video with specified arguments
-def process_video_wrapper(args):
-    return process_video(*args)
+def process_video_wrapper(video, exp_folder, features_folder):
+    return process_video(video, exp_folder, features_folder)
 
 
 def process_video(video, exp_folder, output_folder):
