9f87883c75fdd0fcae07f12680eb16f4c717dc76
campbellcole <10430178+campbellcole@users.noreply.github.com>
2025-02-01 02:36:07 -0600

    change overly optimistic comment

--- palmreader_analysis/common.py
+++ palmreader_analysis/common.py
@@ -1,5 +1,5 @@
 """
-Most features have a 1:1 relationship with some column in the summary CSV. In those cases, it makes the most sense to define the computation for both the feature and the summarized column in the same place. This module has all of those columns.
+Some features have a 1:1 relationship with some column in the summary CSV. In those cases, it makes the most sense to define the computation for both the feature and the summarized column in the same place. This module has all of those columns.
 
 Any features which do not have a single column in the summary are defined in the `features.py` file, and likely have one or more summary columns defined in `summary.py`. This separation is not visible to the user. To get all columns for either features or summary, use `FeaturesContext.get_all_features` or `SummaryContext.get_all_columns`.
 """
