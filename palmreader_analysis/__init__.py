9076bb2cd4805254e69d19ce85a3ac1149ba88ac
campbellcole <10430178+campbellcole@users.noreply.github.com>
2025-02-27 14:35:37 -0600

    fix tqdm monkey patch not being applied

--- palmreader_analysis/__init__.py
+++ palmreader_analysis/__init__.py
@@ -2,5 +2,6 @@
 A Python library for analyzing Blackbox data captured using Palmreader.
 """
 
+from .events import Palmreader, PalmreaderProgress
 from .features import FeaturesContext
 from .summary import SummaryContext
