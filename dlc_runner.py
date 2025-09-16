a9b6362e5a4f961cfb1955eeb2ad60f6be4708a2
Alex Zhang <z.a.zhang29@gmail.com>
2025-09-16 08:58:20 -0400

    change the run_analysis script to be CLI and change the skeleton video generation to fall back to 1 recording at a time, mp4 recordings would saturate the memory and break the code

--- dlc_runner.py
+++ dlc_runner.py
@@ -15,35 +15,6 @@ if info.sysname == "Linux":
     dlc_config_path = r"/home/alex/Documents/DLC/dlc-torch-deployment/config.yaml"
 
 
-selected_folders = []
-
-
-# Function to get the paths to the body videos within the specified folders
-# def get_body_videos(folder_paths):
-#     body_videos = []
-#     for folder_path in folder_paths:
-#         video_subfolder_path = os.path.join(folder_path, 'videos')
-#         if os.path.exists(video_subfolder_path):
-#             videos = [os.path.join(video_subfolder_path, f) for f in os.listdir(video_subfolder_path) if
-#                       f.endswith('_body.avi')]
-#             body_videos.extend(videos)
-#     return body_videos
-
-
-# def get_recording_list(directorys):
-#
-#     recording_list = []
-#
-#     for directory in directorys:
-#         for root, dirs, files in os.walk(directory):
-#             for file in files:
-#                 # file_path = os.path.join(root, file)
-#                 if file.endswith("trans_resize.avi"):
-#                     recording_list.append(root)
-#                     # avi_files.append(os.path.join(root, file))
-#     return recording_list
-
-
 # Function to run DeepLabCut on the specified videos
 #
 # THIS IS AN API ENTRYPOINT! If the signature is modified, ensure api.py matches!
@@ -98,33 +69,18 @@ def generate_skeleton(dlc_config_path, body_videos):
         "rfpaw"
     ]
 
-    deeplabcut.create_labeled_video(
-        dlc_config_path,
-        body_videos,
-        shuffle=0,
-        # displayedbodyparts=bodyparts,
-        filtered=True,
-        draw_skeleton=True,
-        overwrite=True,
-    )
+    # iterate through videos
+    for video in body_videos:
+        deeplabcut.create_labeled_video(
+            dlc_config_path,
+            [video],
+            shuffle=0,
+            # displayedbodyparts=bodyparts,
+            filtered=True,
+            draw_skeleton=True,
+            overwrite=True,
+        )
+
     return
 
-
-# Function to prompt the user to select folders using a GUI dialog
-def select_folders():
-    root = tk.Tk()
-    root.withdraw()
-
-    # Ask the user to select subfolders to process
-    selected_folders = []
-    while True:
-        folder = filedialog.askdirectory(
-            parent=root, title="Select a subfolder to process (Cancel to finish)"
-        )
-        if folder:
-            selected_folders.append(folder)
-        else:
-            break
-
-    return selected_folders
 
--- run_analysis.py
+++ run_analysis.py
@@ -6,22 +6,50 @@ import concurrent.futures
 from joblib import Parallel, delayed
 import sys
 import warnings
+import os
+import argparse
 
 sys.path.append("./preprocess/")
-from FourChamber_split_resize import *
 
+
+def parse_time_bins(bin_str):
+    """
+    Parse a string like "0,-1 3,5 3,-1" into ((0, -1), (3, 5), (3, -1))
+    """
+    bins = []
+    for part in bin_str.split():
+        start, end = part.split(",")
+        bins.append((int(start), int(end)))
+    return tuple(bins)
 
 def main():
+    parser = argparse.ArgumentParser(
+        description="Run palmreader analysis on experiment folder"
+    )
+    parser.add_argument(
+        "--experiment_folder",
+        type=str,
+        required=True,
+        help="Path to the experiment folder",
+    )
+    parser.add_argument(
+        "--num_workers",
+        type=int,
+        default=6,
+        help="Number of worker processes (default: 6, good for 13900K CPU)",
+    )
+    parser.add_argument(
+        "--time_bins",
+        type=str,
+        default="0,-1",
+        help=(
+            "Space-separated list of time bins as start,end (default: '0,-1'). "
+            "Example: '0,-1 0,1 3,5 3,-1'"
+        ),
+    )
 
-    # Ask the user to select subfolder to process
-    root = tk.Tk()
-    root.withdraw()
-
-    # input the path to the experiment folder
-    experiment_folder = select_folder()
-
-    # split and resize the 4chamber recordings
-    # FourChamber_split_resize(experiment_folder, fulres=True)
+    args = parser.parse_args()
+    experiment_folder = args.experiment_folder
 
     experiment_name = os.path.basename(experiment_folder)
     parent_folder = os.path.dirname(experiment_folder)
@@ -42,26 +70,24 @@ def main():
     print(f"In total {len(recording_list)} videos to be processed: ")
     print(f"{[os.path.basename(recording) for recording in recording_list]}")
 
-    num_workers = 6  # set to work with 13900K CPU
     # ignore warnings encountered during the process
     warnings.filterwarnings("ignore", message="Mean of empty slice")
     warnings.filterwarnings("ignore", message="invalid value encountered in scalar divide")
     warnings.filterwarnings("ignore", message="invalid value encountered in arccos")
     warnings.filterwarnings("ignore", message="divide by zero encountered in scalar divide")
-    with concurrent.futures.ProcessPoolExecutor(max_workers=num_workers) as executor:
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers=args.num_workers) as executor:
         futures = [
             executor.submit(process_recording_wrapper, recording)
             for recording in recording_list
         ]
-
-
+        # wait for completion
+        concurrent.futures.wait(futures)
 
     # generate summary csv from the processed videos
-    time_bins = ((0, 5),)
+    time_bins = parse_time_bins(args.time_bins)
     generate_summary_csv(analysis_folder, time_bins)
 
 
 if __name__ == "__main__":
-    import tkinter as tk
-
     main()
